84d98b13dee054a29513521284b8a7231584a174
marlonnbsb <marlonnbsb@gmail.com>
2021-03-08 20:07:22 -0300

    first commit

---
+++ Memoria/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Memoria
+//
+//  Created by Marlon Machado on 08/03/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world teste!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
